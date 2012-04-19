359540e57083d162b15acc523835901d18ef2144
Kuleshov Alexander <kuleshovmail@gmail.com>
2012-04-19 19:08:32 +0600

    get_path_value remade

--- src/mochijson3_helper.erl
+++ src/mochijson3_helper.erl
@@ -10,38 +10,63 @@
 
 -export([get_path_value/2]).
 
-%% @doc Allows extract data from structure from mochijson3:decode
+%% @doc Find value from mochijson3 output
 %% @end
--spec get_path_value(Path :: [binary() | pos_integer()], RD :: any()) -> any() | error.
-get_path_value(Path, RD) ->
-	[Key | Tail] = Path,
-	case RD of
-		{struct, List} when is_binary(Key) ->
-			case get_value(Key, List) of
-				error -> % no such value
-					error;
-				Value ->
-					if
-						Tail == [] ->
-							Value; % this is what we are looking for
-						true -> % path is not empty, we have to continue
-							get_path_value(Tail, Value) % go really deep
-					end
-			end;
-		{array, XList} when is_integer(Key), Key =< length(XList), Key > 0 -> % array case
-			get_path_value(Tail, lists:nth(Key, XList));
-		_ -> % wrong input
-			error
-	end.
+-spec get_path_value(Request :: [{pos_integer(), binary()}] | [], Data :: [{atom(), [{binary(), binary()}, ...]}]) -> binary().
+get_path_value([], Data) ->
+    % if we have no [], we return Data
+    Data;
 
-%% @doc Used to get values from POST requests
-%% @end
--spec get_value(binary(),maybe_improper_list()) -> any().
-get_value(Key, RD) when is_binary(Key) -> % string
-    case lists:keyfind(Key, 1, RD) of
-        false -> error;
-        {Key, Value} -> Value
-    end.
+%% @doc Find value from mochijson3 output
+%% @end       
+get_path_value(Path, RD) when is_list(RD) ->
+    % Check request data length
+    % [{Num, RequestBody}, ...]
+    case length(Path) of
+        1 ->
+            % Get current request body
+            [{Num, NeedToFind}] = Path,
+            % Find in the data structure
+            FindedData = lists:nth(Num, RD),
+            % Get finded data
+            {struct, NeedToFindValue} = FindedData,    
+            % Get value
+            TryGetValue = lists:keyfind(NeedToFind, 1, NeedToFindValue),
+            case TryGetValue of
+                false ->
+                   error;
+                _ ->
+                  {_, Value} = TryGetValue,
+                  Value
+            end;
+        _ ->
+           % Find further
+           get_path_value(Path, lists:nth(1, RD))
+    end;
+
+%% @doc Find value from mochijson3 output
+%% @end  
+get_path_value(Path, RD) when is_tuple(RD) ->
+   % Get data struct body
+   {struct, DataList} = RD,
+   % Get first request body
+   [H | T] = Path,
+   % Find value
+   {Num, NeedFindValue} = H,   
+   List = lists:filter(fun(X) -> 
+              {Value, _} = X,
+              Value == NeedFindValue
+          end,
+          DataList),
+   case List of
+      [] ->
+         error;
+      _ ->
+          % Return value
+          FindedValue = lists:nth(Num, List),
+          {NeedFindValue, OtherValue} = FindedValue,
+          get_path_value(T, OtherValue)
+   end.
 
 %% =======================================================================================
 %% unit tests
@@ -51,104 +76,56 @@ get_value(Key, RD) when is_binary(Key) -> % string
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-get_json_path_value1_test() ->
-
-	Fields = {struct,
-				[{<<"algorithm">>,<<"HMAC-SHA256">>},
-				{<<"credits">>,
-					{struct,
-						[{<<"buyer">>,1027419793},
-						{<<"receiver">>,1027419793},
-						{<<"order_id">>,163932157040089},
-						{<<"order_info">>, <<"{\"title\":\"BFF Locket\",\"description\":\"This is a BFF Locket...\",\"price\":\"10\",\"image_url\":\"http://www.facebook.com/images/gifts/21.png\",\"item_id\":12345,\"product_url\":\"http://www.facebook.com/images/gifts/21.png\"}">>},
-						{<<"test_mode">>,1}]
-					}
-				},
-				{<<"expires">>,1324566000},
-				{<<"issued_at">>,1324558806},
-				{<<"oauth_token", "AAACZAFCZBTVWIBAItMZAwXXrYstGHwkA3winVuj8NUZC5eYYRx2JPME5HsZAyd7yorQrERPElmJwQaSGnq2tg6FZCO0Tqb4EOcL1ZBzQpTrlKXzyhqJg265">>},
-				{<<"user">>,
-					{struct,
-						[{<<"country">>, <<"ru">>},
-						{<<"locale">>,   <<"en_US">>},
-						{<<"age">>,
-							{struct,
-								[{<<"min">>,21}]
-							}
-						}]
-					}
-				},
-				{<<"user_id">>, <<"1027419793">>}]
-			},
-
-	?assertEqual(<<"HMAC-SHA256">>, get_path_value([<<"algorithm">>], Fields)),
-	?assertEqual(1027419793, get_path_value([<<"credits">>, <<"buyer">>], Fields)),
-	?assertEqual(<<"en_US">>, get_path_value([<<"user">>, <<"locale">>], Fields)),
-	?assertEqual(error, get_path_value([<<"user">>, <<"locale1">>], Fields)),
-
-	ok.
-
-get_json_path_value2_test() ->
-
-	Fields = {struct1,
-				[{<<"algorithm">>,<<"HMAC-SHA256">>},
-				{<<"credits">>,
-					{struct3,
-						[{<<"buyer">>,1027419793},
-						{<<"receiver">>,1027419793},
-						{<<"order_id">>,163932157040089},
-						{<<"order_info">>, <<"{\"title\":\"BFF Locket\",\"description\":\"This is a BFF Locket...\",\"price\":\"10\",\"image_url\":\"http://www.facebook.com/images/gifts/21.png\",\"item_id\":12345,\"product_url\":\"http://www.facebook.com/images/gifts/21.png\"}">>},
-						{<<"test_mode">>,1}]
-					}
-				},
-				{<<"expires">>,1324566000},
-				{<<"issued_at">>,1324558806},
-				{<<"oauth_token">>, <<"AAACZAFCZBTVWIBAItMZAwXXrYstGHwkA3winVuj8NUZC5eYYRx2JPME5HsZAyd7yorQrERPElmJwQaSGnq2tg6FZCO0Tqb4EOcL1ZBzQpTrlKXzyhqJg265">>},
-				{<<"user">>,
-					{struct2,
-						[{<<"country">>,<<"ru">>},
-						{<<"locale">>,<<"en_US">>},
-						{<<"age">>,
-							{struct,
-								[{<<"min">>,21}]
-							}
-						}]
-					}
-				},
-				{<<"user_id">>, <<"1027419793">>}]
-			},
-
-	?assertEqual(error, get_path_value([<<"algorithm">>], Fields)),
-	?assertEqual(error, get_path_value([<<"credits">>, <<"buyer">>], Fields)),
-	?assertEqual(error, get_path_value([<<"user">>, <<"locale1">>], Fields)),
-
-	ok.
-
-get_json_path_value3_test() ->
-
-	Fields = {struct,
-					[{<<"content">>,
-						{array,
-							[{struct,
-								[{<<"title">>,<<"BFF Locket">>},
-				                {<<"price">>,<<"10">>},
-				                {<<"description">>,<<"This is a BFF Locket...">>},
-				                {<<"image_url">>,<<"http://www.facebook.com/images/gifts/21.png">>},
-				                {<<"product_url">>, <<"http://www.facebook.com/images/gifts/21.png">>},
-				                {<<"player_id">>,<<"f37c13d279ca301a7e1809175e4b700b">>},
-				                {<<"item_id">>,<<"ITEM_ID_XSCRES">>}]
-				            }]
-				        }
-				    },
-				    {<<"method">>,<<"payments_get_items">>}]
-				},
-
-	?assertEqual(<<"10">>, get_path_value([<<"content">>, 1, <<"price">>], Fields)),
-	?assertEqual(error, get_path_value([<<"content">>, 0, <<"price">>], Fields)),
-	?assertEqual(error, get_path_value([<<"content">>, 2, <<"price">>], Fields)),
-	?assertEqual(error, get_path_value([<<"content">>, 1, <<"price1">>], Fields)),
-	?assertEqual(<<"payments_get_items">>, get_path_value([<<"method">>], Fields)),
-
-	ok.
-	
+get_path_value_test() ->
+    Data1 =
+        {struct, [
+            {<<"resp1">>, <<"resp6">>},
+            {<<"resp2">>, <<"resp4">>},
+            {<<"resp3">>, <<"resp5">>}
+        ]},
+        
+    ?assertEqual(<<"resp4">>, get_path_value([{1, <<"resp2">>}], Data1)),
+    
+    Data2 =
+        {struct, [
+            {<<"resp1">>, <<"resp6">>},
+            {<<"resp2">>, <<"resp4">>},
+            {<<"resp3">>, <<"resp5">>},
+            {<<"resp2">>, <<"aaaaaaaaaaaaaa">>}   
+        ]},
+    
+    ?assertEqual(<<"aaaaaaaaaaaaaa">>, get_path_value([{2, <<"resp2">>}], Data2)),
+            
+    Data3 =
+        {struct,[
+            {<<"1">>, <<"2">>},
+            {<<"response">>, [
+                {struct,[
+                    {<<"uid">>,1103656},
+                    {<<"first_name">>,<<"Max">>},
+                    {<<"last_name">>,<<"Bourinov">>},
+                    {<<"response2">>, [
+                        {struct,[
+                            {<<"uid">>,1103656},
+                            {<<"first_name">>,<<"Max">>},
+                            {<<"last_name">>,<<"Bourinov">>}
+                        ]},
+                        {struct,[
+                            {<<"uid">>,1103656},
+                            {<<"first_name">>,<<"Sasha">>},
+                            {<<"last_name">>,<<"Kuleshov">>}
+                        ]}
+                   ]}     
+                 ]},
+                 {struct,[
+                     {<<"uid">>,1103656},
+                     {<<"first_name">>,<<"Max">>},
+                     {<<"last_name">>,<<"Bourinov">>}
+                 ]}
+             ]}
+        ]},
+        
+    ?assertEqual(<<"Max">>, get_path_value([{1, <<"response">>}, {1, <<"response2">>}, {1, <<"first_name">>}], Data3)),
+        
+    ok.
 -endif.
